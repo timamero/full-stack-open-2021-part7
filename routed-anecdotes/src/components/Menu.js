79f63fa156d3495cb07c5180563f6bfd50645c09
Fatima Camero <fcamero12@gmail.com>
2021-11-01 21:09:50 -0700

    refactored code and separated views into components

---
+++ routed-anecdotes/src/components/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Menu = () => {
+  console.log('menu render')
+  const padding = {
+    paddingRight: 5
+  }
+  return (
+    <div>
+      <Link style={padding} to="/">Anecdotes</Link>
+      <Link style={padding} to="/create">Create New</Link>
+      <Link style={padding} to="/about">About</Link>
+    </div>
+  )
+}
+
+export default Menu
