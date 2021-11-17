0317b37b5882b8ff8c08cfcec5f75e9ae93146c8
Fatima Camero <fcamero12@gmail.com>
2021-11-16 16:49:47 -0800

    create useCountry custom hook and refactor code to get data to display

---
+++ country-hook/src/App.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+  console.log('in useCountry')
+  useEffect(() => {
+    if (name) {
+      console.log('sending request')
+      const request = axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      // .then(response => response)
+      request
+        .then(response => setCountry(response.data[0]))
+        .catch(err => setCountry({name: {common: null}}))
+
+    } 
+  }, [name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.name.common) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.name.common} </h3>
+      <div>capital {country.capital[0]} </div>
+      <div>population {country.population}</div> 
+      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/>  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
