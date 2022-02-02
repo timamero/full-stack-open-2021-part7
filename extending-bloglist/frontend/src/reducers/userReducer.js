25d123b15980adfbf48bf3fb5501ee233e862e90
Fatima Camero <fcamero12@gmail.com>
2022-02-02 10:57:58 -0800

    create userReducer

---
+++ extending-bloglist/frontend/src/reducers/userReducer.js
@@ -0,0 +1,28 @@
+const userReducer = (state = [], action) => {
+  switch(action.type) {
+  case 'INIT_USER':
+    console.log('initialize user', action.data.user)
+    return action.data.user
+  case 'LOGOUT_USER':
+    return null
+  default:
+    return state
+  }
+}
+
+export const initializeUser = (user) => {
+  return {
+    type: 'INIT_USER',
+    data: {
+      user
+    }
+  }
+}
+
+export const logoutUser = () => {
+  return {
+    type: 'LOGOUT_USER',
+  }
+}
+
+export default userReducer
