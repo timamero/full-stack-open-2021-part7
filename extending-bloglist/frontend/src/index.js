4d2359c3bdc465945194d73b4df04f85b080e0d8
Fatima Camero <fcamero12@gmail.com>
2022-02-02 11:03:38 -0800

    use combineReducer to add multiple reducers

--- extending-bloglist/frontend/src/index.js
+++ extending-bloglist/frontend/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import blogReducer from './reducers/blogReducer'
+import userReducer from './reducers/userReducer'
 
-const store = createStore(blogReducer)
+const reducer = combineReducers({
+  blogs: blogReducer,
+  user: userReducer
+})
+const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider store={store}>
