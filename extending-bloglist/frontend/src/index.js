360bf62ab13864f0f5e2ebd92d0497f16fc0ac1a
Fatima Camero <fcamero12@gmail.com>
2022-02-04 15:04:36 -0800

    add notificationReducer to store

--- extending-bloglist/frontend/src/index.js
+++ extending-bloglist/frontend/src/index.js
@@ -5,10 +5,12 @@ import { Provider } from 'react-redux'
 import App from './App'
 import blogReducer from './reducers/blogReducer'
 import userReducer from './reducers/userReducer'
+import notificationReducer from './reducers/notificationReducer'
 
 const reducer = combineReducers({
   blogs: blogReducer,
-  user: userReducer
+  user: userReducer,
+  notification: notificationReducer
 })
 const store = createStore(reducer)
 
