f2daef81ace3e5bddffe74fc472a7aff2c107949
Fatima Camero <fcamero12@gmail.com>
2022-02-09 20:56:39 -0800

    add thead and tbody to table in Users page

--- extending-bloglist/frontend/src/pages/Users.js
+++ extending-bloglist/frontend/src/pages/Users.js
@@ -8,19 +8,23 @@ const Users = () => {
     <div>
       <h2>Users</h2>
       <table>
-        <tr>
-          <th></th>
-          <th>Blogs Created</th>
-        </tr>
-        {users.map(user => {
-          return (
-            <tr key={user.id}>
-              <td >{user.name}</td>
-              <td >{user.blogs.length}</td>
-            </tr>
-          )}
-        )
-        }
+        <thead>
+          <tr>
+            <th></th>
+            <th>Blogs Created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => {
+            return (
+              <tr key={user.id}>
+                <td >{user.name}</td>
+                <td >{user.blogs.length}</td>
+              </tr>
+            )}
+          )
+          }
+        </tbody>
       </table>
     </div>
   )
