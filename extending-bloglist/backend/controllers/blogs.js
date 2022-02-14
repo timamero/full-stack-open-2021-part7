66dcea0bdbedbd697f63c21051979e56b0608bd9
Fatima Camero <fcamero12@gmail.com>
2022-02-14 07:56:17 -0800

    add post comments to blog in backend

--- extending-bloglist/backend/controllers/blogs.js
+++ extending-bloglist/backend/controllers/blogs.js
@@ -2,11 +2,13 @@ const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
 const User = require('../models/user')
+const Comment = require('../models/comment')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
     .find({})
     .populate('user', { username: 1, name: 1 })
+    .populate('comments')
   response.json(blogs)
 })
 
@@ -92,6 +94,38 @@ blogsRouter.put('/:id', async (request, response, next) => {
   } catch(exception) {
     next(exception)
   }  
+})
+
+blogsRouter.post('/:id/comments', async (request, response) => {
+  const body = request.body
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token is missing or invalid' })
+  }
+
+  if (request.token) {
+    const id = request.params.id
+    request.blog = await Blog.findById(id)
+  }
+
+  const blog = request.blog
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const comment = new Comment({
+    content: body.content,
+  })
+  
+  const savedComment = await comment.save()
+  blog.comments = blog.comments.concat(savedComment._id)
+  await blog.save()
+
+  response.status(200).json(savedComment)
 })
 
 module.exports = blogsRouter
