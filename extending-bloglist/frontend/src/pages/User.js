6d8913ac6f554f1f90214b7e35188ec68577fced
Fatima Camero <fcamero12@gmail.com>
2022-02-21 07:10:24 -0800

    update User and Users styling

--- extending-bloglist/frontend/src/pages/User.js
+++ extending-bloglist/frontend/src/pages/User.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import { Card, CardBody, CardSubtitle, CardTitle, List } from 'reactstrap'
 
 const User = () => {
   const id = useParams().id
@@ -11,13 +12,19 @@ const User = () => {
   }
 
   return (
-    <div>
-      <h2>{user.name}</h2>
-      <h3>Added Blogs</h3>
-      <ul>
-        {user.blogs.map(blog => <li key={blog.id}>{blog.title}</li>)}
-      </ul>
-    </div>
+    <Card>
+      <CardBody>
+        <CardTitle tag="h2">
+          {user.name}
+        </CardTitle>
+        <CardSubtitle tag="h3" className="mb-2 text-muted">
+          Added Blogs
+        </CardSubtitle>
+        <List type="unstyled">
+          {user.blogs.map(blog => <li key={blog.id} className="py-1">{blog.title}</li>)}
+        </List>
+      </CardBody>
+    </Card>
   )
 }
 
--- extending-bloglist/frontend/src/pages/Users.js
+++ extending-bloglist/frontend/src/pages/Users.js
@@ -1,33 +1,39 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { Card, CardBody, CardTitle, Table } from 'reactstrap'
 
 const Users = () => {
   const users = useSelector(state => state.users)
 
   return (
-    <div>
-      <h2>Users</h2>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Blogs Created</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => {
-            return (
-              <tr key={user.id}>
-                <td><Link to={`/users/${user.id}`}>{user.name}</Link></td>
-                <td>{user.blogs.length}</td>
-              </tr>
-            )}
-          )
-          }
-        </tbody>
-      </table>
-    </div>
+    <Card>
+      <CardBody>
+        <CardTitle tag="h2">
+          Users
+        </CardTitle>
+        <Table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Blogs Created</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => {
+              return (
+                <tr key={user.id}>
+                  <td><Link to={`/users/${user.id}`}>{user.name}</Link></td>
+                  <td>{user.blogs.length}</td>
+                </tr>
+              )}
+            )
+            }
+          </tbody>
+        </Table>
+      </CardBody>
+
+    </Card>
   )
 }
 
