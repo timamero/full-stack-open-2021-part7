dc590273b4665da9281d08a61a7e02c15f44dcc0
Fatima Camero <fcamero12@gmail.com>
2022-02-21 06:57:49 -0800

    update BlogList styling

--- extending-bloglist/frontend/src/components/BlogList.js
+++ extending-bloglist/frontend/src/components/BlogList.js
@@ -6,17 +6,18 @@ import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { createBlog } from '../reducers/blogReducer'
 import { setInfoMessage, setErrorMessage } from '../reducers/notificationReducer'
+import { Card } from 'reactstrap'
 
 const BlogList = ({ blogs }) => {
   const dispatch = useDispatch()
 
-  const blogStyle = {
-    display: 'block',
-    border: '1px solid black',
-    borderRadius: '0.2rem',
-    margin: '1rem 0',
-    padding: '0.5rem'
-  }
+  // const blogStyle = {
+  //   display: 'block',
+  //   border: '1px solid black',
+  //   borderRadius: '0.2rem',
+  //   margin: '1rem 0',
+  //   padding: '0.5rem'
+  // }
 
   const blogFormRef = useRef()
 
@@ -40,7 +41,9 @@ const BlogList = ({ blogs }) => {
         <CreateBlog handleCreateBlog={handleCreateBlog}/>
       </Togglable>
       {blogs && blogs.map(blog =>
-        <Link key={blog.id} to={`/blogs/${blog.id}`} style={blogStyle}>{blog.title}</Link>
+        <Card key={blog.id} className="my-2" body>
+          <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+        </Card>
       )}
     </div>
   )
